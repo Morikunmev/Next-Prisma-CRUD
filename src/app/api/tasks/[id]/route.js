4c547b26193e36bb06006c21414927b8918e6d7e
Richard <ricky201325@gmail.com>
2025-06-21 05:39:01 -0300

    backend rest api

---
+++ src/app/api/tasks/[id]/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../libs/prisma";
+
+export async function GET(request, { params }) {
+  const task = await prisma.task.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  return NextResponse.json(task);
+}
+
+export async function PUT(request, { params }) {
+  const data = await request.json();
+  const taskUpdated = await prisma.task.update({
+    where: { id: parseInt(params.id) },
+    data: data,
+  });
+  return NextResponse.json(taskUpdated);
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = await params;
+
+    await prisma.task.delete({
+      where: { id: parseInt(id) },
+    });
+
+    return NextResponse.json({ message: "Tarea eliminada" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error al eliminar tarea" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,16 @@
 import { NextResponse } from "next/server";
+import { prisma } from "../../../libs/prisma";
 
-export function GET(request) {
-  return NextResponse.json("Creando tareas");
+export async function GET() {
+  const tasks = await prisma.task.findMany();
+  return NextResponse.json(tasks);
+}
+
+export async function PUT(request, { params }) {
+  const { id } = await params; //const data = await request.json();
+  const taskUpdated = await prisma.task.update({
+    where: { id: parseInt(id) }, // 
+    data: data,
+  });
+  return NextResponse.json(taskUpdated);
 }
